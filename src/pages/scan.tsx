508dba0576c3ae6e7de37dda89eb90101c775ed7
R1013-T <nosuke1013@icloud.com>
2023-05-06 11:18:21 +0900

    done

--- src/pages/scan.tsx
+++ src/pages/scan.tsx
@@ -8,6 +8,7 @@ import * as THREE from "three";
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const scan = () => {
+  // 順番大事です。
   const markers = ["lock", "dent"];
 
   const [foundMarker, setFoundMarker] = useState("");
