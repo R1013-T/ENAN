388af51b0e7407708f47fff174ba5fbb9c78661a
R1013-T <nosuke1013@icloud.com>
2023-07-19 17:36:45 +0900

    Update default title in Layout component
    The default title in the Layout component was modified from "名探偵エナン" to "WebAR推理ゲーム". This is part of our ongoing effort to internationalize and align with the recent rebranding of the company.

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useEffect } from "react";
+import { FC, ReactNode } from "react";
 import Head from "next/head";
 import Header from "./Header";
 import UnderButton from "./underButton/UnderButton";
@@ -14,33 +14,33 @@ type Title = {
 
 export const Layout: FC<Title> = (props) => {
   return (
-    <div className="h-screen bg-bg-black overflow-hidden">
+    <div className="h-screen overflow-hidden bg-bg-black">
       <Head>
         <title>{`${
           props.title ? props.title + " - " : ""
-        } 名探偵エナン`}</title>
+        } WebAR推理ゲーム`}</title>
       </Head>
       {props.headerType === "top" ? (
         <>
           <header className="h-2/5">
             <Header headerType={props.headerType} />
           </header>
-          <main className="h-3/5 grid place-items-center">
+          <main className="grid h-3/5 place-items-center">
             <div className="h-full w-full max-w-md">{props.children}</div>
           </main>
         </>
       ) : (
         <>
-          <header className="absolute top-0 left-0 z-40">
+          <header className="absolute left-0 top-0 z-40">
             <Header headerType={props.headerType} />
           </header>
           <main className="main">
-            <div className="h-full w-full max-w-md relative">
+            <div className="relative h-full w-full max-w-md">
               {props.children}
             </div>
             {props.hideUnderButton ? null : <UnderButton />}
             {props.isLoading ? (
-              <div className="fixed z-30 top-0 right-0 left-0 bottom-0 grid place-items-center bg-black/20">
+              <div className="fixed bottom-0 left-0 right-0 top-0 z-30 grid place-items-center bg-black/20">
                 <BiLoader className="rotationInfinite " size={20} />
               </div>
             ) : (
