c0cf2d3a9877e16eaa5859583bb7c3d6ca722c25
R1013-T <nosuke1013@icloud.com>
2023-07-19 17:31:16 +0900

    Update Header component with new images and styles
    Reworked the Header component to include new images and adjustments to div positions and image sizes for improved display on multiple page types. The changes aim to provide better user experience and aesthetic consistency across the application.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -7,26 +7,33 @@ interface Props {
 
 const Header = (props: Props) => {
   return (
-    <div className="h-full relative">
-      <div className="fixed z-40 top-6 right-6">
+    <div className="relative h-full">
+      <div className="fixed right-6 top-6 z-40">
         <VscMenu size={25} className="text-gray-300" />
       </div>
       {props.headerType === "top" ? (
-        <div className="absolute bottom-12 right-1/2 translate-x-1/2">
+        <div className="absolute -bottom-7 right-1/2 flex translate-x-1/2 flex-wrap items-center justify-center">
           <Image
-            src="/images/enan_logo_mini.png"
-            alt="enan"
+            src="/images/webar.png"
+            alt="WebAR推理ゲーム"
             width="250"
             height="200"
+            className="mb-8 w-full"
+          />
+          <Image
+            src="/images/enan_icon.png"
+            alt={"enan_icon"}
+            width="130"
+            height="130"
           />
         </div>
       ) : (
-        <div className="fixed z-40 top-6 left-6">
+        <div className="fixed left-6 top-7 z-40">
           <Image
-            src="/images/enan_logo_mini.png"
-            alt="enan"
-            width="70"
-            height="100"
+            src="/images/webar.png"
+            alt="WebAR推理ゲーム"
+            width="100"
+            height="130"
           />
         </div>
       )}
